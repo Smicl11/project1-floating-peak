33001e79cdc255709db8b9dfc8fcc1904ca995c4
smicl11 <bryan.smith094@gmail.com>
2016-04-08 12:34:09 -0700

    updated front end and added footer button

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,21 +1,34 @@
 /* CLIENT-SIDE JS */
 
 $(document).ready(function() {
-  $.ajax({
-    method: "GET",
-    url: "api/sanity",
-    success: sanitySuccess,
-    error: sanityError
+  $('#photo-share form').on('submit', function(event){
+    event.preventDefault();
+    $.ajax({
+      method: 'POST',
+      url: '/api/photos/',
+      data: $(this).serialize(),
+      success: handleSuccess,
+      error: handleError
+    });
+    $(this).trigger("reset");
   });
 
 });
 
-function sanitySuccess(json) {
-  console.log("you did it!");
-  console.log(json);
+
+
+function handleSuccess(json) {
+  json.forEach(renderHTML);
 }
 
-function sanityError(error) {
+function handleError(error) {
   console.log("DAMMIT! ERROR!!");
   console.log(error);
+}
+
+function renderHTML(pics) {
+  var source = $('#pic-template').html();
+  var template = Handlebars.compile(source);
+  var newHTML = template(pics);
+  $('#feed').prepend(newHTML);
 }
