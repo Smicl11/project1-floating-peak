80482ba8a07d9be1046b270f6192a05a9e5a4dcb
smicl11 <bryan.smith094@gmail.com>
2016-04-10 21:01:26 -0700

    added event listener for save button and function to handle event

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -4,7 +4,6 @@ $(document).ready(function() {
   //grab data from the server to display on the page
   renderPhotos();
 
-  //capture data from user submission and POST it to the site.
   $('#photo-share form').on('submit', function(event){
     event.preventDefault();
     $.ajax({
@@ -18,13 +17,51 @@ $(document).ready(function() {
   });
 
   $('#photos').on('click', '.delete-photo', handleDelete);
-
   $('#photos').on('click', '.edit-photo', handleEdit);
+  $('#photos').on('click', '.save-photo', saveEdits);
 
 });
 
 
+function handleEdit(event) {
+  var currentPost = $(this).closest('.photo-post');
+  var photoId = currentPost.data('photo-id');
+/* REMOVE */console.log('edit photo', photoId);
 
+  currentPost.find('.save-photo').toggleClass('hidden');
+  currentPost.find('.edit-photo').toggleClass('hidden');
+
+  var title = currentPost.find('h3.title').text();
+  currentPost.find('h3.title').html('<input class="edit-title" value="' + title + '"></input>');
+
+  var about = currentPost.find('p.about').text();
+  currentPost.find('p.about').html('<input class="edit-about" value="' + about + '"></input>');
+}
+
+function saveEdits(event) {
+  var photoId = $(this).closest('.photo-post').data('photo-id');
+  var currentPost = $('[data-photo-id=' + photoId + ']');
+
+  var data = {
+    title: currentPost.find('.edit-title').val(),
+    about: currentPost.find('.edit-about').val(),
+  };
+  /* REMOVE */console.log('PUTing data for album', photoId, 'with data', data);
+
+  $.ajax({
+    method: 'PUT',
+    url: '/api/photos/' + photoId,
+    data: data,
+    success: updatePhoto
+  });
+}
+
+function updatePhoto (data) {
+  var photoId = data._id;
+  $('[data-photo-id=' + photoId + ']').remove();
+  renderPhotos(data);
+  $('[data-photo-id=' + photoId + ']')[0].scrollIntoView();
+}
 
 function renderPhotos() {
   $.ajax({
@@ -43,8 +80,8 @@ function renderHTML(pics) {
 }
 
 function handleDelete(event) {
-  var photoId = $(this).parents('.photo-post').data('photo-id');
-  console.log('someone wants to delete photo id=' + photoId ); //remove this when issue resolved
+  var photoId = $(this).closest('.photo-post').data('photo-id');
+  /* REMOVE WHEN ISSUE RESOLVED*/  console.log('someone wants to delete photo id=' + photoId );
   $.ajax({
     url: '/api/photos/' + photoId,
     method: 'DELETE',
@@ -54,7 +91,7 @@ function handleDelete(event) {
 }
 
 function handleDeletePhotoSuccess(data) {
-  console.log(data); //remove this
+  /* REMOVE */  console.log(data);
   var deletedPhotoId = data._id;
   $('div[data-photo-id=' + deletedPhotoId + ']').remove();
 }
@@ -72,5 +109,6 @@ function handleSuccess(json) {
 
 function handleError(error) {
   console.log("DAMMIT! ERROR!! ", error);
-  alert("Sorry, something went wrong. Please try again later."); //change this to a modal in the future
+  //Change the alert into a modal if possible within time contraints
+  alert("Sorry, something went wrong. Please try again later.");
 }
