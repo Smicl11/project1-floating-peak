2b0bebcc9166b8f0480f0b3285f3bf70561ef4e1
smicl11 <bryan.smith094@gmail.com>
2016-04-12 12:02:52 -0700

    corrected error causing server to crash

--- models/index.js
+++ models/index.js
@@ -1,7 +1,7 @@
 var mongoose = require("mongoose");
 mongoose.connect( process.env.MONGOLAB_URI ||
                   process.env.MONGOHQ_URL ||
-                  "YOUR CURRENT LOCALHOST DB CONNECTION STRING HERE" );
+                  "mongodb://localhost/project1-floating-peak" );
 
 
 
